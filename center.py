f0972180b59f87e33a7d1c7e0e8ce74e0c2da0a2
孙中杰 <MIDASmidas75@qq.com>
2024-08-19 14:06:07 +0000

    重构center，修改测试样例

--- center.py
+++ center.py
@@ -12,130 +12,127 @@ from persistence import IPersistence, MemoryPersistence
 from config_loader import ConfigLoader
 
 
-instances: dict[str, set[str]] = {}
-unique: dict[str, Instance] = {}
-wsInstance: dict[str, WebSocket] = {}
-app = FastAPI()
-app.mount(
-    "/static",
-    StaticFiles(directory=f"{os.path.dirname(__file__)}/static"),
-    name="static",
-)
-persistence: IPersistence = MemoryPersistence()
-configLoader = ConfigLoader()
-
-
-async def login(instance: Instance, websocket: WebSocket) -> bool:
-    uid = str(instance)
-    if uid in unique:
-        return False
-    unique[uid] = instance
-    if instance.serviceName in instances:
-        instances[instance.serviceName].add(uid)
-    else:
-        instances[instance.serviceName] = {uid}
-    wsInstance[uid] = websocket
-    return True
-
-
-async def logout(instance: Union[Instance, str]) -> bool:
-    uid = str(instance)
-    if uid not in unique:
-        return False
-    unique.pop(uid)
-    instances[instance.serviceName].remove(uid)
-    await persistence.removeLog(uid)
-    wsInstance.pop(uid)
-    return True
-
-
-async def log(instance: Instance, body: dict, websocket: WebSocket) -> None:
-    uid = str(instance)
-    await persistence.addLog(uid, body["message"])
-
-
-async def route(instance: Instance, body: dict, websocket: WebSocket) -> None:
-    result = instances.copy()
-    for key in instances:
-        result[key] = {}
-        for uid in instances[key]:
-            result[key][uid] = unique[uid].model_dump()
-    payload = {"type": "route", "data": result}
-    await websocket.send_json(payload)
-
-
-async def config(instance: Instance, body: dict, websocket: WebSocket) -> None:
-    payload = {"type": "config", "data": {}}
-    if "config" in body and body["config"] is not None:
-        payload["data"][body["config"]] = configLoader.config[body["config"]]
-    else:
-        payload["data"] = configLoader.config
-    await websocket.send_json(payload)
-
-
-wsCallable: dict[str, Callable[[Instance, dict[str, Any, WebSocket]], None]] = {
-    "log": log,
-    "route": route,
-    "config": config,
-}
-
-
-@app.websocket("/sws")
-async def websocketEndpoint(websocket: WebSocket):
-    try:
-        instance = Instance(
-            serviceName=websocket.headers.get("Service-Name"),
-            hostname=websocket.headers.get("Hostname"),
-            port=int(websocket.headers.get("Port")),
+class PySCDLCenter:
+    def __init__(self, host: str = "0.0.0.0", port: int = 3100):
+        self.instances = dict[str, set[str]]()
+        self.unique = dict[str, Instance]()
+        self.wsInstance = dict[str, WebSocket]()
+        self.serverConfig = uvicorn.Config(app=FastAPI(),host=host,port=port)
+        self.serverConfig.app.mount(
+            "/static",
+            StaticFiles(directory=f"{os.path.dirname(__file__)}/static"),
+            name="static",
         )
-        await websocket.accept()
-        await login(instance, websocket)
-        while True:
-            data = await websocket.receive_json()
-            await wsCallable[data["type"]](instance, data, websocket)
-    except WebSocketDisconnect:
-        await logout(instance)
+        self.persistence: IPersistence = MemoryPersistence()
+        self.configLoader = ConfigLoader()
+        self.wsCallable: dict[
+            str, Callable[[Instance, dict[str, Any, WebSocket]], None]
+        ] = {
+            "log": self.log,
+            "route": self.route,
+            "config": self.config,
+        }
 
+        @self.serverConfig.app.websocket("/sws")
+        async def websocketEndpoint(websocket: WebSocket):
+            try:
+                instance = Instance(
+                    serviceName=websocket.headers.get("Service-Name"),
+                    hostname=websocket.headers.get("Hostname"),
+                    port=int(websocket.headers.get("Port")),
+                )
+                await websocket.accept()
+                await self.login(instance, websocket)
+                while True:
+                    data = await websocket.receive_json()
+                    await self.wsCallable[data["type"]](instance, data, websocket)
+            except WebSocketDisconnect:
+                await self.logout(instance)
 
-@app.get("/config/reload")
-async def reloadConfig():
-    configLoader.loadConfig()
-    return responses.JSONResponse({"msg": "ok"})
+        @self.serverConfig.app.get("/config/reload")
+        async def reloadConfig():
+            self.configLoader.loadConfig()
+            return responses.JSONResponse({"msg": "ok"})
 
+        @self.serverConfig.app.post("/config/save/{firstKey}")
+        async def saveConfig(firstKey: str, reqeustBody: dict):
+            self.configLoader.saveConfig(firstKey, reqeustBody)
+            return responses.JSONResponse({"msg": "ok"})
 
-@app.post("/config/save/{firstKey}")
-async def saveConfig(firstKey: str, reqeustBody: dict):
-    configLoader.saveConfig(firstKey, reqeustBody)
-    return responses.JSONResponse({"msg": "ok"})
+        @self.serverConfig.app.get("/config/list")
+        async def listConfig():
+            return responses.JSONResponse(
+                {"data": list(self.configLoader.config.keys())}
+            )
 
+        @self.serverConfig.app.get("/config/get/{firstKey}")
+        async def getConfig(firstKey: str):
+            if firstKey in self.configLoader.config:
+                return responses.JSONResponse(
+                    {"data": self.configLoader.config[firstKey]}
+                )
+            else:
+                return responses.JSONResponse({"data": "{}"})
 
-@app.get("/config/list")
-async def listConfig():
-    return responses.JSONResponse({"data": list(configLoader.config.keys())})
+        @self.serverConfig.app.get("/list")
+        async def getList():
+            return responses.JSONResponse({"data": list(self.unique.keys())})
 
+        @self.serverConfig.app.get("/log/{uid}")
+        async def getLog(uid: str):
+            return responses.JSONResponse({"data": await self.persistence.getLog(uid)})
 
-@app.get("/config/get/{firstKey}")
-async def getConfig(firstKey: str):
-    if firstKey in configLoader.config:
-        return responses.JSONResponse({"data": configLoader.config[firstKey]})
-    else:
-        return responses.JSONResponse({"data": "{}"})
+        @self.serverConfig.app.delete("/logout/{uid}")
+        async def deleteById(uid: str):
+            return responses.JSONResponse(
+                {"message": "success" if self.logout(uid) else "failed"}
+            )
 
+    async def login(self, instance: Instance, websocket: WebSocket) -> bool:
+        uid = str(instance)
+        if uid in self.unique:
+            return False
+        self.unique[uid] = instance
+        if instance.serviceName in self.instances:
+            self.instances[instance.serviceName].add(uid)
+        else:
+            self.instances[instance.serviceName] = {uid}
+        self.wsInstance[uid] = websocket
+        return True
 
-@app.get("/list")
-async def getList():
-    return responses.JSONResponse({"data": list(unique.keys())})
+    async def logout(self, instance: Union[Instance, str]) -> bool:
+        uid = str(instance)
+        if uid not in self.unique:
+            return False
+        self.unique.pop(uid)
+        self.instances[instance.serviceName].remove(uid)
+        await self.persistence.removeLog(uid)
+        self.wsInstance.pop(uid)
+        return True
 
+    async def log(self, instance: Instance, body: dict, websocket: WebSocket) -> None:
+        uid = str(instance)
+        await self.persistence.addLog(uid, body["message"])
 
-@app.get("/log/{uid}")
-async def getLog(uid: str):
-    return responses.JSONResponse({"data":await persistence.getLog(uid)})
+    async def route(self, instance: Instance, body: dict, websocket: WebSocket) -> None:
+        result = self.instances.copy()
+        for key in self.instances:
+            result[key] = {}
+            for uid in self.instances[key]:
+                result[key][uid] = self.unique[uid].model_dump()
+        payload = {"type": "route", "data": result}
+        await websocket.send_json(payload)
 
-
-@app.delete("/logout/{uid}")
-async def deleteById(uid: str):
-    return responses.JSONResponse({"message": "success" if logout(uid) else "failed"})
+    async def config(
+        self, instance: Instance, body: dict, websocket: WebSocket
+    ) -> None:
+        payload = {"type": "config", "data": {}}
+        if "config" in body and body["config"] is not None:
+            payload["data"][body["config"]] = self.configLoader.config[body["config"]]
+        else:
+            payload["data"] = self.configLoader.config
+        await websocket.send_json(payload)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3100)
+    uvicorn.Server(PySCDLCenter().serverConfig).run()
--- test/testReconnect.py
+++ test/testReconnect.py
@@ -1,12 +1,31 @@
 import sys
 import os
+import threading
+import uvicorn
+import time
 
 sys.path.append(f"{os.path.dirname(__file__)}/..")
 from client import WebSocketClient
 from base_model import ServerConfig, Instance
+from center import PySCDLCenter
 
+center = PySCDLCenter()
+
+
+def createAThread() -> uvicorn.Server:
+    server = uvicorn.Server(center.serverConfig)
+    t = threading.Thread(target=server.run, daemon=True)
+    t.start()
+    return server
+
+
+s = createAThread()
 c = WebSocketClient(
     serverConfig=ServerConfig(host="127.0.0.1", port=3100),
     instance=Instance(serviceName="test", hostname="127.0.0.1", port=8100),
 )
-input()
+while True:
+    input("press any key to restart server")
+    s.handle_exit(None, None)
+    time.sleep(2)
+    s = createAThread()
